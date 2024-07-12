aa3dcac79a85f5e2fb269636dd20a4f426609660
yuichi04 <crgone.910428.en@gmail.com>
2024-07-12 21:44:16 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Auth } from './components/Auth'
+import { Todo } from './components/Todo'
+import axios from 'axios'
+import { CsrfToken } from './types'
+
+function App() {
+  useEffect(() => {
+    axios.defaults.withCredentials = true
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`
+      )
+      axios.defaults.headers.common['X-CSRF-TOKEN'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
